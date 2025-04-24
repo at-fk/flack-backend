2d76d46bacf0783874a9e85e4539c59c0ffb0216
Atsu Fukushima <64303487+at-fk@users.noreply.github.com>
2025-04-23 17:31:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 quotes = [
     "The only limit to our realization of tomorrow is our doubts of today.",
